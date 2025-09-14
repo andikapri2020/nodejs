a00fda114853751dea8ae81b8303136ab66f195c
AVBK305VA-AK\ITSEC <andikapri87@gmail.com>
2025-09-14 23:32:02 +0700

    ok gass

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      // Tambahkan aturan kustom Anda di sini
+      // Misalnya:
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"]
+    }
+  }
+];
